13953450fdd388286570757d4c6d975c5aa206f8
Lauriel Mesquita <laurielmesquita@me.com>
2014-05-02 19:58:46 -0300

    Form buid done

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ $(window).scroll(function() {
 		$('.main-header').removeClass('show-main-header');
 	}
 });
-
 $('.nav').onePageNav({
 	currentClass: 'active',
 	changeHash: false,
@@ -15,7 +14,6 @@ $('.nav').onePageNav({
 	filter: '',
 	easing: 'swing',
 });
-
 $('.navbar h1 a, .jumbotron p a, #sobre a, #servicos a').smoothScroll({
 	speed: 750,
 });
@@ -23,41 +21,40 @@ $('.navbar h1 a, a.topp').smoothScroll({
 	offset: -79,
 	speed: 1250,
 });
-
 $(document).ready(function() {
-		$(window).bind('load', function() {
-			parallaxInit();
-		});
-
-		function parallaxInit() {
-			testMobile = isMobile.any();
-			if (testMobile == null) {
-				$('#sobre .well').parallax("50%", 0.1);
-				$('#servicos .well').parallax("50%", 0.1);
-				$('#equipamentos .well').parallax("50%", 0.1);
-			}
-		}
+	$(window).bind('load', function() {
 		parallaxInit();
 	});
-	//Mobile Detect
-	var testMobile;
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-		BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-		iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-		Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-		Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-		any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+
+	function parallaxInit() {
+		testMobile = isMobile.any();
+		if (testMobile == null) {
+			$('#sobre .well').parallax("50%", 0.1);
+			$('#servicos .well').parallax("50%", 0.1);
+			$('#equipamentos .well').parallax("50%", 0.1);
 		}
-	};
+	}
+	parallaxInit();
+});
+//Mobile Detect
+var testMobile;
+var isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function() {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	}
+};
